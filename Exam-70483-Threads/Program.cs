c6256f34407364072d224a3008ec4e09f9e52839
raakeysh <raakesh.raut@gmail.com>
2017-08-26 23:58:35 -0400

    new console project

---
+++ Exam-70483-Threads/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam_70483_Threads
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine("Inside main {0}", i );
+                Thread.Sleep(0);
+            }
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+        }
+
+        
+    }
+}
--- Exam-70483/Threads.cs
+++ Exam-70483/Threads.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Exam_70483
 {
     public class Threads
     {
+
     }
 }
